0a1547024f755fc97460f580ba3e41f5ff04ec4a
Arata <aratadayoon@yahoo.co.jp>
2025-12-27 00:03:44 +0900

    Revert "docker-compose.yamlにWebRTC UDPポートを追加し、TCPポートの設定を更新。synapseサービスを追加。"
    This reverts commit 17e21799c0f495635472473cebeb8bc75ef46f6f.

--- terraform/ec2/sg.tf
+++ terraform/ec2/sg.tf
@@ -3,6 +3,13 @@ resource "aws_security_group" "workadventure_sg" {
   description = "workadventure_sg"
 
   ingress {
+    from_port   = 4173
+    to_port     = 4173
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
