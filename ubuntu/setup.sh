4d75826b204773b7d6608bda8463eca57b303437
Arata <aratadayoon@yahoo.co.jp>
2025-12-30 15:44:43 +0900

    Node.jsとdotenvxのインストール手順をsetup.shに追加

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,6 +1,14 @@
+# Ubuntu
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg make
 sudo install -m 0755 -d /etc/apt/keyrings
+
+# Node.js
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo npm install -g @dotenvx/dotenvx
+
+# Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
@@ -9,7 +17,4 @@ echo \
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker ubuntu
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt install -y nodejs
-sudo npm install -g @dotenvx/dotenvx
 newgrp docker
