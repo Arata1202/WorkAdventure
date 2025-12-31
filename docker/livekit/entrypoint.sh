86af6d4b0fa1426e4126b2ef5b5ab6941f5a569b
Arata <aratadayoon@yahoo.co.jp>
2025-12-31 10:08:08 +0900

    entrypoint.shのsedコマンドでの変数展開の構文を修正

--- docker/livekit/entrypoint.sh
+++ docker/livekit/entrypoint.sh
@@ -8,8 +8,8 @@ set -eu
 if [ ! -f /tmp/livekit-config.yaml ] || [ /etc/livekit/livekit-config.template.yaml -nt /tmp/livekit-config.yaml ]; then
     echo "Generating livekit-config.yaml from livekit-config.template.yaml..."
     sed \
-    -e "s/\$${LIVEKIT_API_KEY}/$${LIVEKIT_API_KEY}/g" \
-    -e "s/\$${LIVEKIT_API_SECRET}/$${LIVEKIT_API_SECRET}/g" \
+    -e "s|\${LIVEKIT_API_KEY}|$LIVEKIT_API_KEY|g" \
+    -e "s|\${LIVEKIT_API_SECRET}|$LIVEKIT_API_SECRET|g" \
     /etc/livekit/livekit-config.template.yaml > /tmp/livekit-config.yaml
 fi
 
