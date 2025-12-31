3aff5300dc3c62331a0dc939cd53fac29c474b16
Arata <aratadayoon@yahoo.co.jp>
2025-12-31 22:32:02 +0900

    add: postgres

--- synapse/entrypoint.sh
+++ synapse/entrypoint.sh
@@ -6,6 +6,9 @@ set -eu
 : "${MATRIX_REGISTRATION_SHARED_SECRET:?Error: MATRIX_REGISTRATION_SHARED_SECRET is not set}"
 : "${MATRIX_MACAROON_SECRET_KEY:?Error: MATRIX_MACAROON_SECRET_KEY is not set}"
 : "${MATRIX_FORM_SECRET:?Error: MATRIX_FORM_SECRET is not set}"
+: "${POSTGRES_DB:?Error: POSTGRES_DB is not set}"
+: "${POSTGRES_USER:?Error: POSTGRES_USER is not set}"
+: "${POSTGRES_PASSWORD:?Error: POSTGRES_PASSWORD is not set}"
 : "${OPENID_CLIENT_ID:?Error: OPENID_CLIENT_ID is not set}"
 : "${OPENID_CLIENT_SECRET:?Error: OPENID_CLIENT_SECRET is not set}"
 
@@ -16,6 +19,9 @@ if [ ! -f /data/homeserver.yaml ] || [ /homeserver.template.yaml -nt /data/homes
     -e "s|\${MATRIX_REGISTRATION_SHARED_SECRET}|$MATRIX_REGISTRATION_SHARED_SECRET|g" \
     -e "s|\${MATRIX_MACAROON_SECRET_KEY}|$MATRIX_MACAROON_SECRET_KEY|g" \
     -e "s|\${MATRIX_FORM_SECRET}|$MATRIX_FORM_SECRET|g" \
+    -e "s|\${POSTGRES_DB}|$POSTGRES_DB|g" \
+    -e "s|\${POSTGRES_USER}|$POSTGRES_USER|g" \
+    -e "s|\${POSTGRES_PASSWORD}|$POSTGRES_PASSWORD|g" \
     -e "s|\${OPENID_CLIENT_ID}|$OPENID_CLIENT_ID|g" \
     -e "s|\${OPENID_CLIENT_SECRET}|$OPENID_CLIENT_SECRET|g" \
     /homeserver.template.yaml > /data/homeserver.yaml
