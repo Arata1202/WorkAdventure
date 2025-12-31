617606ed8a23495ef962a6b37f7196e9dd540a8f
Arata <aratadayoon@yahoo.co.jp>
2025-12-31 21:45:52 +0900

    Revert "feat: mysql"
    This reverts commit eafc83e6ceec79f0bfd04d536af423385983d545.

--- synapse/entrypoint.sh
+++ synapse/entrypoint.sh
@@ -6,9 +6,6 @@ set -eu
 : "${MATRIX_REGISTRATION_SHARED_SECRET:?Error: MATRIX_REGISTRATION_SHARED_SECRET is not set}"
 : "${MATRIX_MACAROON_SECRET_KEY:?Error: MATRIX_MACAROON_SECRET_KEY is not set}"
 : "${MATRIX_FORM_SECRET:?Error: MATRIX_FORM_SECRET is not set}"
-: "${MYSQL_DATABASE:?Error: MYSQL_DATABASE is not set}"
-: "${MYSQL_USER:?Error: MYSQL_USER is not set}"
-: "${MYSQL_PASSWORD:?Error: MYSQL_PASSWORD is not set}"
 : "${OPENID_CLIENT_ID:?Error: OPENID_CLIENT_ID is not set}"
 : "${OPENID_CLIENT_SECRET:?Error: OPENID_CLIENT_SECRET is not set}"
 
@@ -19,9 +16,6 @@ if [ ! -f /data/homeserver.yaml ] || [ /homeserver.template.yaml -nt /data/homes
     -e "s|\${MATRIX_REGISTRATION_SHARED_SECRET}|$MATRIX_REGISTRATION_SHARED_SECRET|g" \
     -e "s|\${MATRIX_MACAROON_SECRET_KEY}|$MATRIX_MACAROON_SECRET_KEY|g" \
     -e "s|\${MATRIX_FORM_SECRET}|$MATRIX_FORM_SECRET|g" \
-    -e "s|\${MYSQL_DATABASE}|$MYSQL_DATABASE|g" \
-    -e "s|\${MYSQL_USER}|$MYSQL_USER|g" \
-    -e "s|\${MYSQL_PASSWORD}|$MYSQL_PASSWORD|g" \
     -e "s|\${OPENID_CLIENT_ID}|$OPENID_CLIENT_ID|g" \
     -e "s|\${OPENID_CLIENT_SECRET}|$OPENID_CLIENT_SECRET|g" \
     /homeserver.template.yaml > /data/homeserver.yaml
