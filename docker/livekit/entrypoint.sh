4d62930627e242b64e3901fb71a85fe47ee4ffd4
Arata <aratadayoon@yahoo.co.jp>
2025-12-31 09:49:17 +0900

    livekitサービスのエントリポイントをスクリプトに変更し、設定ファイルの生成処理を追加

---
+++ docker/livekit/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+
+: "${LIVEKIT_API_SECRET:?Error: LIVEKIT_API_SECRET is not set}"
+
+if [ ! -f /tmp/livekit-config.yaml ] || [ /etc/livekit/livekit-config.template.yaml -nt /tmp/livekit-config.yaml ]; then
+    echo "Generating livekit-config.yaml from livekit-config.template.yaml..."
+    sed -e "s/\$${LIVEKIT_API_SECRET}/$${LIVEKIT_API_SECRET}/g" \
+        /etc/livekit/livekit-config.template.yaml > /tmp/livekit-config.yaml
+fi
+
+exec /livekit-server --config /tmp/livekit-config.yaml
