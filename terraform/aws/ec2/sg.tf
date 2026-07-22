ab83a484cd6ad8490fe0fed21af07abb30cf5dad
Arata <aratadayoon@yahoo.co.jp>
2026-07-22 23:40:22 +0900

    chore: remove commented-out ingress rule for SSH in security group

--- terraform/aws/ec2/sg.tf
+++ terraform/aws/ec2/sg.tf
@@ -2,13 +2,6 @@ resource "aws_security_group" "workadventure_sg" {
   name        = "workadventure_sg"
   description = "workadventure_sg"
 
-  # ingress {
-  #   from_port   = 22
-  #   to_port     = 22
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
   ingress {
     from_port   = 80
     to_port     = 80
